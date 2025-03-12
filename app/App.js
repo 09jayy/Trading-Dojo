e21aa1c519ca5310ee9abcddca6e34bb11df1195
jibril <100243649+09jayy@users.noreply.github.com>
2025-03-12 08:44:46 +0000

    created app context for signed in so can track throughout app whether user is signed in

--- app/App.js
+++ app/App.js
@@ -1,11 +1,38 @@
-import { StyleSheet, Text, View} from 'react-native';
+import {useState} from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { SignRoot } from './screens/Sign/SignRoot';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { signedInContext } from './AppContext';
+import { SignIn } from './screens/Sign/SignIn';
+import { SignUp } from './screens/Sign/SignUp';
+import { Dashboard } from './screens/Dashboard/Dashboard';
+
+const Tab = createBottomTabNavigator(); 
 
 export default function App() {
+  const [signedIn, setSignedIn] = useState(false)
+  
   return (
-    <NavigationContainer>
-      <SignRoot/>
-    </NavigationContainer>
+    <signedInContext.Provider value={{signedIn,setSignedIn}}>
+      <NavigationContainer>
+        {
+          signedIn == false ? (
+            <Tab.Navigator
+            screenOptions={
+                {
+                    headerShown: false
+                }
+            }
+            >
+                    <Tab.Screen name='SignIn' component={SignIn}/>
+                    <Tab.Screen name='SignUp' component={SignUp}/>
+            </Tab.Navigator>
+          ) : (
+            <Tab.Navigator>
+              <Tab.Screen name='Dashboard' component={Dashboard}/>
+            </Tab.Navigator>
+          )
+        }
+      </NavigationContainer>
+    </signedInContext.Provider>
   );
 }
