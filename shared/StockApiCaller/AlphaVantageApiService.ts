dadb9d5a9200f1d731c8c512c4be819c259107fe
jibril <100243649+09jayy@users.noreply.github.com>
2025-03-19 23:50:09 +0000

    added methods for todo classes

--- shared/StockApiCaller/AlphaVantageApiService.ts
+++ shared/StockApiCaller/AlphaVantageApiService.ts
@@ -1,4 +1,7 @@
 import {ApiService} from './ApiService'; 
 
 export class AlphaVantageApiService implements ApiService {
+    fetchLatestTradePriceOf(symbol: string, apiKey: string, secretKey: string): Promise<number> {
+        throw new Error('Method not implemented.');
+    }
 }
--- shared/StockApiCaller/YahooFinanceApiService.ts
+++ shared/StockApiCaller/YahooFinanceApiService.ts
@@ -1,5 +1,7 @@
 import {ApiService} from './ApiService'; 
 
 export class YahooFinanceApiService implements ApiService {
-
+    fetchLatestTradePriceOf(symbol: string, apiKey: string, secretKey: string): Promise<number> {
+        throw new Error('Method not implemented.');
+    }
 }
