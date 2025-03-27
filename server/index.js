eaf15a33f4ccb3613f51eccef0c247f59667c818
jibril <100243649+09jayy@users.noreply.github.com>
2025-03-27 03:49:06 +0000

    npm install StockApiCaller in server and created endpoint for stockdata using yahoo finance, server doubles as a proxy for yahoo finance due to incompatibility issues when used directly in react native expo

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const StockApiCaller = require('stockapicaller'); 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/stock/:symbol', async (req, res) => {
+    const symbol = req.params.symbol;
+    const stockApiCaller = new StockApiCaller().setApiService('yahoofinance');
+    const stats = await stockApiCaller.fetchStatsOf(symbol);
+    res.send(stats);
 })
 
 app.listen(port, () => {
