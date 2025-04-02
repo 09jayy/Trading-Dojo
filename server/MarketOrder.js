3d7a0b19fb97613ebf4dc97f5c19466b525374f2
jibril <100243649+09jayy@users.noreply.github.com>
2025-04-03 00:04:11 +0100

    init methods for marketOrder

--- server/MarketOrder.js
+++ server/MarketOrder.js
@@ -1,7 +1,35 @@
 const IOrderStrategy = require('./IOrderStrategy.js')
 
 class MarketOrder extends IOrderStrategy {
+    constructor({tradeType, stockSymbol, shareQuantity, userId}){
+        this.tradeType = tradeType; 
+        this.stockSymbol = stockSymbol;
+        this.shareQuantity = shareQuantity; 
+        this.userId = userId; 
+    }
 
+    execute() {
+        switch (this.tradeType) {
+            case 'buy':
+                this.buy(); 
+                break;
+            case 'sell':
+                this.sell();
+                break; 
+        }
+    }
+
+    getTradeType() {
+        return this.tradeType; 
+    }
+
+    buy() {
+        
+    }
+
+    sell() {
+
+    }
 }
 
 module.exports = MarketOrder;
