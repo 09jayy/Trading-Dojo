4c44e29279d7d89172059e0fc5b328c264a4a6df
AndyS005 <andisilisteanu@gmail.com>
2025-04-08 20:43:53 +0100

    added glossary

---
+++ app/screens/Glossary/GlossaryStyle.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: '#f0f0f0',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+  term: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 5,
+  },
+  definition: {
+    fontSize: 16,
+    color: '#555',
+    lineHeight: 22,
+  },
+});
