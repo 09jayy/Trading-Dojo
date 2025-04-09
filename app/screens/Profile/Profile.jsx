209fb461833d987948f2d21b9bf47c3616f1f853
Batuhan Ali Ozturk <154116402+BatuhanAli@users.noreply.github.com>
2025-04-09 16:38:37 +0100

    updating user details + ui implementation finished

--- app/screens/Profile/Profile.jsx
+++ app/screens/Profile/Profile.jsx
@@ -1,16 +1,42 @@
 import { useContext, useState } from 'react';
-import {View, Text, TouchableOpacity, TextInput} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput, Modal, Alert} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './ProfileStyles';
 import { signedInContext } from '../../AppContext';
-import { update, signOutHandler } from './functions/ProfileFunctions';
+import { updateEmailHandler, updateUsername, updatePasswordHandler, signOutHandler } from './functions/ProfileFunctions';
 
 export const Profile = () => {
-    const {setSignedIn} = useContext(signedInContext)
+    const {setSignedIn} = useContext(signedInContext);
     const [email, setEmail] = useState("")
+    const [oldEmail, setOldEmail] = useState("")
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
+    const [oldPassword, setOldPassword] = useState("")
+    const [ usernameModalVisible, setUsernameModalVisible] = useState(false)
+    const [ emailModalVisible, setEmailModalVisible] = useState(false)
+    const [ passwordModalVisible, setPasswordModalVisible] = useState(false)
+
+    const handleUsernameChange = async () => {
+        await updateUsername(username);
+        setUsernameModalVisible(false);
+        setUsername("");
+    }
+
+    const handleEmailChange = async () => {
+        await updateEmailHandler(email, oldEmail, password);
+        setEmailModalVisible(false);
+        setEmail("");
+        setOldEmail("");
+        setPassword("");
+    }
+
+    const handlePasswordChange = async () => {
+        await updatePasswordHandler(email, oldPassword, password);
+        setPasswordModalVisible(false);
+        setPassword("");
+        setOldPassword("");
+        setEmail("");
+    }
 
     return (
         <SafeAreaView style={styles.container}>
@@ -26,46 +52,115 @@ export const Profile = () => {
                     </View>
                 </View>
 
-                <TextInput
-                    style={styles.input}
-                    placeholder="Email..."
-                    placeholderTextColor="#888"
-                    value={email}
-                    onChangeText={setEmail}
-                />
+                <TouchableOpacity style={styles.saveButton} onPress={()=> setEmailModalVisible(true)}>
+                    <Text style={styles.buttonText}>Update Email</Text>
+                </TouchableOpacity>
 
-                <TextInput
-                    style={styles.input}
-                    placeholder="Username..."
-                    placeholderTextColor="#888"
-                    value={username}
-                    onChangeText={setUsername}
-                />
+                <TouchableOpacity style={styles.saveButton} onPress={()=> setUsernameModalVisible(true)}>
+                    <Text style={styles.buttonText}>Update Username</Text>
+                </TouchableOpacity>
 
-                <TextInput
-                    style={styles.input}
-                    placeholder='Password...'
-                    placeholderTextColor="#888"
-                    value={password}
-                    onChangeText={setPassword}
-                />
-
-                <TextInput
-                    style={styles.input}
-                    placeholder='Confirm Password...'
-                    placeholderTextColor="#888"
-                    value={confirmPassword}
-                    onChangeText={setConfirmPassword}
-                />
+                <TouchableOpacity style={styles.saveButton} onPress={()=> setPasswordModalVisible(true)}>
+                    <Text style={styles.buttonText}>Update Password</Text>
+                </TouchableOpacity>
             </View>
-
-            <TouchableOpacity style={styles.saveButton} onPress={()=>update(email, username, password, confirmPassword)}>
-                <Text style={styles.buttonText}>Save</Text>
-            </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} onPress={()=>signOutHandler(setSignedIn)}>
                 <Text style={styles.buttonText}>Sign Out</Text>
             </TouchableOpacity>
+
+            <Modal visible={usernameModalVisible} animationType="slide" transparent>
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalBox}>
+                        <Text style={styles.modalTitle}>Update Username</Text>
+                        <TextInput
+                            style={styles.inputModal}
+                            placeholder="Enter new username..."
+                            value={username}
+                            onChangeText={setUsername}
+                        />
+                        <View style={styles.modalButtonRow}>
+                            <TouchableOpacity style={styles.modalButton} onPress={() => handleUsernameChange()}>
+                                <Text style={styles.buttonText}>Confirm</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.modalButton} onPress={() => setUsernameModalVisible(false)}>
+                                <Text style={styles.buttonText}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal visible={emailModalVisible} animationType="slide" transparent>
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalBox}>
+                        <Text style={styles.modalTitle}>Update Email</Text>
+                        <TextInput
+                            style={styles.inputModal}
+                            placeholder="Enter new Email..."
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                        <TextInput
+                            style={styles.inputModal}
+                            placeholder="Enter old Email..."
+                            value={oldEmail}
+                            onChangeText={setOldEmail}
+                        />
+                        <TextInput
+                            style={styles.inputModal}
+                            placeholder="Enter Password..."
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+
+                        <View style={styles.modalButtonRow}>
+                            <TouchableOpacity style={styles.modalButton} onPress={() => handleEmailChange()}>
+                                <Text style={styles.buttonText}>Confirm</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.modalButton} onPress={() => setEmailModalVisible(false)}>
+                                <Text style={styles.buttonText}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
+            <Modal visible={passwordModalVisible} animationType="slide" transparent>
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalBox}>
+                        <Text style={styles.modalTitle}>Update Password</Text>
+                        <TextInput
+                            style={styles.inputModal}
+                            placeholder="Enter Email..."
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                        <TextInput
+                            style={styles.inputModal}
+                            placeholder="Enter old Password..."
+                            value={oldPassword}
+                            onChangeText={setOldPassword}
+                        />
+                        <TextInput
+                            style={styles.inputModal}
+                            placeholder="Enter New Password..."
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+
+                        <View style={styles.modalButtonRow}>
+                            <TouchableOpacity style={styles.modalButton} onPress={() => handlePasswordChange()}>
+                                <Text style={styles.buttonText}>Confirm</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.modalButton} onPress={() => setPasswordModalVisible(false)}>
+                                <Text style={styles.buttonText}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
         </SafeAreaView>
     )
 }
--- app/screens/Profile/functions/ProfileFunctions.jsx
+++ app/screens/Profile/functions/ProfileFunctions.jsx
@@ -1,21 +1,9 @@
 import { Alert } from 'react-native';
-import { signOut, updateEmail, updatePassword, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
+import { signOut, updateEmail, updatePassword, reauthenticateWithCredential, EmailAuthProvider, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth, db } from '../../../components/Config/firebaseConfig';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { updateDoc, doc } from 'firebase/firestore';
-
-export const update = async (email, username, password, confirmPassword) => {
-    user = await getUser();
-    uid = user.uid;
-    if (uid === null) {
-        Alert.alert("Error: ", "We cannot update at this time, please try again later", [{ text: "Understood" , onPress: () => console.log("Alert closed")}]);
-        return;
-    }
-    
-    updateEmailHandler(email);
-    updateUsername(username, uid);
-    updatePasswordHandler(password, confirmPassword);
-}
+import { useState } from 'react';
 
 const getUser = async () => {
     try {
@@ -32,16 +20,10 @@ const getUser = async () => {
     }
 }
 
-const updateEmailHandler = (email, user) => {
-    console.log("email: ", email);
+export const updateUsername = async (username) => {
+    const user = await getUser();
+    const uid = user.uid;
 
-    if (email !== ""){
-                    Alert.alert("Your email has been updated to: ", email);
-            }
-}
-
-const updateUsername = (username, uid) => {
-    console.log("username: ", username);
     if (username !== "") {
         const userRef = doc(db, "users", uid);
         try {
@@ -53,14 +35,44 @@ const updateUsername = (username, uid) => {
             Alert.alert("Error: ", "Could not update username, please try again later", [{ text: "Understood" , onPress: () => console.log("Alert closed")}]);
             console.log("Error: ", error.message);
         }
+    } else {
+        Alert.alert("Please enter a valid username");
+        return;
     }
 }
 
-const updatePasswordHandler = (password, confirmPassword) => {
-    console.log("password: ", password)
-    console.log("confirmation: ", confirmPassword)
-    if((password === confirmPassword) && (password !== "")){
-        Alert.alert("Your password has been updated to: ", password);
+export const updateEmailHandler = async (email, oldEmail, password) => {
+    if (email !== ""){
+        try {
+            await signInWithEmailAndPassword(auth, oldEmail, password);
+            const credential = EmailAuthProvider.credential(oldEmail, password);
+            const account = auth.currentUser
+            await reauthenticateWithCredential(account, credential);
+            await updateEmail(account, email);
+            Alert.alert("Your email has been updated to: ", email);
+        } catch(error) {
+            Alert.alert("Error: ", "We couldn't update your email at this time please try again later", [{ text: "Understood" , onPress: () => console.log("Alert closed")}]);
+            console.log("Error: ", error.message);
+        }
+    }
+}
+
+export const updatePasswordHandler = async (email, oldPassword, password) => {
+    if (password === "" || password.length < 6) {
+        Alert.alert("Please enter a valid password longer than six characters");
+        return;
+    }
+
+    try {
+        await signInWithEmailAndPassword(auth, email, oldPassword);
+        const credential = EmailAuthProvider.credential(email, oldPassword);
+        const account = auth.currentUser
+        await reauthenticateWithCredential(account, credential);
+        await updatePassword(account, password);
+        Alert.alert("Your password has been updated");
+    } catch(error) {
+        Alert.alert("Error: ", "We couldn't update your password at this time please try again later", [{ text: "Understood" , onPress: () => console.log("Alert closed")}]);
+        console.log("Error: ", error.message);
     }
 }
 
